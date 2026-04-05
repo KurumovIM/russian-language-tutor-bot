627e81ac9a544d8d8bdf81a4175b2452ad159e05
KurumovIM <kurumovila@gmail.com>
2026-04-05 18:44:03 +0300

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,9 @@
+"""
+ÐÐ»Ð°Ð²Ð¸Ð°ÑÑÑÑ Ð¸ ÐºÐ½Ð¾Ð¿ÐºÐ¸ Ð´Ð»Ñ Ð±Ð¾ÑÐ°
+Keyboards and buttons for the bot
+"""
+
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+# ==================== ÐÐ¡ÐÐÐÐÐ«Ð ÐÐÐÐÐÐÐ¢Ð£Ð Ð« ====================
